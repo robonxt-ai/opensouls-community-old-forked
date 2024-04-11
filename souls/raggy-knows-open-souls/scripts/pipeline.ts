c18c3fc0479f5ec03dee7d8f35170ce432769cd5
Topper Bowers <topper@toppingdesign.com>
2024-04-11 19:44:33 +0200

    simplified raggy pipeline (#57)

---
+++ souls/raggy-knows-open-souls/scripts/pipeline.ts
@@ -0,0 +1,3 @@
+import run from "../src/pipeline/run.js"
+
+await run()
--- souls/raggy-knows-open-souls/src/pipeline/run.ts
+++ souls/raggy-knows-open-souls/src/pipeline/run.ts
@@ -1,45 +1,14 @@
-#!/usr/bin/env npx tsx
-
-import { promises as fs } from 'fs';
-import * as glob from 'glob';
 import { splitSections } from './sectionSplitter.js';
-import { fileURLToPath } from 'url';
-import { dirname, join, relative } from 'path';
+import { FilePipeline } from '@opensouls/engine';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-
-async function processRagDirectory() {
-  console.log("path: ", `${__dirname}/../../docs/**/*`)
-  const files = glob.sync(`${__dirname}/../../docs/**/*`);
-
-  const rootDir = join(__dirname, '..', '..', 'docs');
-
-  const storeDirRoot = join(__dirname, '..', '..', 'stores', 'docs')
-
-  await fs.mkdir(storeDirRoot, { recursive: true });
-
-  for (const file of files) {
-    console.log("file: ", file)
-    try {
-      const content = await fs.readFile(file, 'utf8');
-      const sections = splitSections(content);
-      console.log(`File: ${file} has been split into ${sections.length} sections.`);
-      
-    let sectionNumber = 0;
-    for (const section of sections) {
-      const sectionFileName = `${relative(rootDir, file).replace(/[\/\\]/, '_')}__${sectionNumber}`;
-      const sectionFilePath = join(storeDirRoot, sectionFileName);
-      await fs.writeFile(sectionFilePath, section, 'utf8');
-      console.log(`Section ${sectionNumber} of file ${file} saved as ${sectionFileName}`);
-      sectionNumber++;
-    }
-
-    } catch (err) {
-      console.error(`Error processing file ${file}:`, err);
-    }
-  }
+const run = async () => {
+  const pipeline = new FilePipeline("docs", "stores/docs", { replace: true })
+  return pipeline.process(async ({ content, path }) => {
+    const sections = splitSections(await content());
+    console.log(`File: ${path} has been split into ${sections.length} sections.`);
+   
+    return sections
+  })
 }
 
-processRagDirectory();
+export default run
