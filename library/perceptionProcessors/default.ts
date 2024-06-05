6d77220cb98ad025c9b28ce242b4d0536c3928f2
Topper Bowers <topper@toppingdesign.com>
2024-06-05 10:47:55 +0200

    remove premonition code - OPE-535 (#108)
    * remove premonition code
    
    * Update library/perceptionProcessors/default.ts
    
    Co-authored-by: Thiago Duarte <371426+dooart@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Thiago Duarte <371426+dooart@users.noreply.github.com>

--- library/perceptionProcessors/default.ts
+++ library/perceptionProcessors/default.ts
@@ -7,12 +7,8 @@ function safeName(name?: string) {
   return (name || "").replace(/[^a-zA-Z0-9_-{}]/g, '_').slice(0, 62);
 }
 
-const DEFAULT_PREMONITION = "remembered its time to"
-
 const defaultPerceptionProcessor: PerceptionProcessor = async ({ perception, workingMemory, currentProcess }) => {
-  const content = perception.internal ?
-  `${perception.name} ${perception.premonition || DEFAULT_PREMONITION} ${perception.action} ${perception.content}` :
-  `${perception.name} ${perception.action}: ${perception.content}`
+  const content = `${perception.name} ${perception.action}: "${perception.content}"`
 
   workingMemory = workingMemory.withMemory({
     role: perception.internal ? ChatMessageRoleEnum.Assistant : ChatMessageRoleEnum.User,
